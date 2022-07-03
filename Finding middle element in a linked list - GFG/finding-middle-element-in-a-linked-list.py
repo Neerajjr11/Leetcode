d95ffd4ef8b6030a6b7ee8107905b6dfb03b1a76
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-07-03 11:55:03 +0530

    Added solution - LeetHub

---
+++ Finding middle element in a linked list - GFG/finding-middle-element-in-a-linked-list.py
@@ -0,0 +1,82 @@
+# your task is to complete this function
+
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    #  Should return data of middle node. If linked list is empty, then  -1
+    def findMid(self, head):
+        # Code here
+        # return the value stored in the middle node
+        if head == None:
+            return -1
+
+        check = head
+        l = []
+        counter = 0
+        while check:
+            check = check.next
+            counter += 1
+        counter = (counter//2) 
+        index = 0
+        while head:
+            if index < counter:
+                head = head.next
+                index +=1
+            else:
+                break
+        return head.data
+
+
+#{ 
+#  Driver Code Starts
+# Initial Template for Python3
+
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.tail = self.head
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+def printlist(head):
+    temp = head
+    while temp is not None:
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        ob = Solution()
+        print(ob.findMid(list1.head))
+
+
+# } Driver Code Ends
