cf88fc0a778ba8df44e379ea2672137f16f49a01
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-07-07 18:42:04 +0530

    Time: 59 ms (54.72%), Space: 13.9 MB (56.83%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -2,10 +2,15 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 0 or x == 1:
             return x
-        loop = 1 
-        op = 1
-        while op<=x:
-            
-            loop += 1
-            op = loop * loop
-        return loop-1
+        left = 0
+        right = x
+        while left <= right:
+            mid = left + (right-left) // 2
+            square = mid * mid
+            if square == x:
+                return mid
+            if square < x:
+                left =mid + 1
+            else:
+                right = mid -1
+        return left -1
