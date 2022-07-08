bf29e648fe297fd1bf510f8e0543cfd5f10f3585
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-07-08 22:03:11 +0530

    Time: 626 ms (17.93%), Space: 21.8 MB (25.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,4 +1,5 @@
 class Solution:
+    
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
             return 0
@@ -6,17 +7,18 @@ class Solution:
         row = len(grid)
         col = len(grid[0])
         count = 0 
-        
-        
-        def islands(r,c):
-            if (r not in range(row) or c not in range(col) or grid[r][c] == '0' or (r,c) in visited):
-                return 0
-            visited.add((r,c))
-            
-            return (1 + islands(r+1,c) + islands(r,c+1) + islands(r-1,c) + islands(r,c-1))
         for r in range(row):
             for c in range(col):
-                if islands(r,c)!=0:
+                if self.islands(r,c,grid,visited)!=0:
                     count+=1
         return count
+        
+    def islands(self,r,c,grid,visited):
+        
+        if (r not in range(len(grid)) or c not in range(len(grid[0])) or grid[r][c] == '0' or (r,c) in visited):
+            return 0
+        visited.add((r,c))
+
+        return (1 + self.islands(r+1,c,grid,visited) + self.islands(r,c+1,grid,visited) + self.islands(r-1,c,grid,visited) + self.islands(r,c-1,grid,visited))
+        
         
