abb1ad6ad303654595cb3b02fedf8697862c29ac
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-08-15 03:11:04 +0530

    Time: 575 ms (22.47%), Space: 13.7 MB (98.51%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        s = ""
+        def helper(arr,i,s):
+            if i == len(arr):
+                freq = [0] * 26
+                for k in range(len(s)):
+                    if freq[abs(ord(s[k]) - 97)] == 1:
+                        return 0
+                    freq[abs(ord(s[k]) - 97)] +=1
+                return len(s)
+            op1 = op2 = 0
+            if len(s) + len(arr[i]) <=26:
+                op1 = helper(arr,i+1,s+arr[i])
+            op2 = helper(arr,i+1,s)
+            return max(op1,op2)
+        v= helper(arr,0,s)
+        return v
