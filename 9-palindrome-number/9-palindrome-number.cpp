6ceee6a7fb85cac0420542be559b3875fd312285
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-08-29 20:52:30 +0530

    Time: 12 ms (87.74%), Space: 6.1 MB (10.18%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         string y = to_string(x);
         string n = to_string(x);
         reverse(y.begin(),y.end());
