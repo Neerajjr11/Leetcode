9312e09f7586086591ced4ee19c7da269935c5cb
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-09-11 09:55:22 +0530

    Time: 8 ms (89.37%), Space: 13 MB (18.44%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n = nums.size();
         for(int i=0;i<n;i++){
             nums.push_back(nums[i]);
