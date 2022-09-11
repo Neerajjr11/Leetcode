1bc4ef4918a1c1d4d5ac759423904fa05fa05c0b
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-09-11 09:47:28 +0530

    Time: 3 ms (81.68%), Space: 8.7 MB (37.34%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -9,11 +9,7 @@ public:
         int sum = 0;
         res.push_back(nums[0]);
         for (int i=1;i<n;i++){
-            for(int j=0;j<=i;j++){
-                sum +=nums[j];
-            }
-            res.push_back(sum);
-            sum=0;
+            res.push_back(res.back() + nums[i]);
         }
         return res;
     }
