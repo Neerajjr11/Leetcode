5dafdcd9e50f5ff27534a4c07b73d4cd8c69c3e8
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-10-26 17:04:18 +0530

    Time: 4 ms (57.65%), Space: 8 MB (22.91%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        map<int,int> m;
+        for(int i = 0; i < nums.size(); i++){
+            m[nums[i]]++;
+        }
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(m[nums[i]] == 1)
+                sum+=nums[i];
+        }
+        return sum;
+    }
+};
