00a6d22838af0b6c1dee74e3a1f05a1d8cd1a0f8
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-10-31 09:54:12 +0530

    Time: 39 ms (21.35%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -3,6 +3,7 @@ public:
 	bool isToeplitzMatrix(vector<vector<int>>& matrix) {
 		int row = matrix.size();
 		int col = matrix[0].size();
+//      for the rows > 0 and cols > 0 we just check if each element is equal to the element to the left         and up
 		for (int r = 1; r < row; r++) {
 			for (int c = 1; c < col; c++) {
 				if (matrix[r][c] != matrix[r - 1][c - 1])
