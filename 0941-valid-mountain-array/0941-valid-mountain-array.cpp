ed5ef071a7517b05734e7c1e1afdfecbf4c9dc52
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-11-08 19:51:04 +0530

    Time: 30 ms (92.99%), Space: 22.4 MB (48.08%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -4,24 +4,13 @@ public:
         int n = arr.size();
         if(n < 3)
             return false;
-        int stop = 0;
-        for(int i = 0;i<n-1;i++){
-            if(arr[i] < arr[i+1])
-                stop++;
-            else
-                break;
-        }
-        int c2 = 0;
-        for(int i =stop; i<n-1;i++){
-            if(arr[i] <= arr[i+1])
-                return false;
-            else
-                c2++;
-        }
-        if(stop == n-1)
+        int i = 1;
+        while(i < n && (arr[i-1]<arr[i]))
+            i+=1;
+        if(i==n || i == 1)
             return false;
-        else if(c2 == n-1)
-            return false;
-        return true;
+        while(i<n && (arr[i-1] > arr[i]))
+            i+=1;
+        return (i==n);
     }
 };
