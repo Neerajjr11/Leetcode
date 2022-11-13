ce245fcf241c9ad64d488199bdc85d75078282b2
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-11-13 15:12:28 +0530

    Time: 3 ms (93.72%), Space: 7.1 MB (50.06%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.cpp
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -7,8 +7,8 @@ public:
         char temp = 'a';
         for(int i = 0;i<key.length();i++){
             if(mp.find(key[i]) == mp.end()){
-            mp[key[i]] = temp;
-            temp++;
+                mp[key[i]] = temp;
+                temp++;
             }
         }
         for(int i = 0; i < message.length(); i++)
