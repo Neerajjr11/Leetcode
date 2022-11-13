6c16e39f03a5cd6321916a61b35ee05247324047
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-11-13 15:12:17 +0530

    Time: 4 ms (85.77%), Space: 7.2 MB (25.18%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        key.erase(remove(key.begin(), key.end(), ' '), key.end());
+        string res = "";
+        map<char,char>mp;
+        char temp = 'a';
+        for(int i = 0;i<key.length();i++){
+            if(mp.find(key[i]) == mp.end()){
+            mp[key[i]] = temp;
+            temp++;
+            }
+        }
+        for(int i = 0; i < message.length(); i++)
+        {
+            if(message[i] == ' ')
+                res += " ";
+            else
+            {
+                res += mp[message[i]];
+            }
+        }
+        return res;
+    }
+};
