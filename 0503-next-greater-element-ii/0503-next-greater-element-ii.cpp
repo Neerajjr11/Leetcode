ac2f3850178ac72abfcdfd92d4bb0b54cb0fce12
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-12-08 20:05:06 +0530

    Time: 38 ms (92.40%), Space: 23.9 MB (79.21%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int maxidx = max_element(nums.begin(), nums.end()) - nums.begin();
-        int maxi = *max_element(nums.begin(), nums.end());
-        vector<int> res;
-        int flag = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == maxi)
-                res.push_back(-1);
-            for(int j = i+1; j < nums.size(); j++){
-                if(nums[i] < nums[j]){
-                    res.push_back(nums[j]);
-                    flag = 1;
-                    break;
-
-                }
+        stack<int> stack;
+        int n = nums.size();
+        int idx;
+        vector<int> res(n, -1);
+        for(int i = 0; i < 2*n; i++){
+            if(i >=n)
+                idx = i - n;
+            else
+                idx = i;
+            while(!stack.empty() && nums[idx] > nums[stack.top()])
+            {
+                res[stack.top()] = nums[idx];
+                stack.pop();
             }
-            if(flag == 0){
-                for(int k = 0; k < maxidx+1; k++){
-                    if(nums[k] > nums[i]){
-                        res.push_back(nums[k]);
-                        break;
-                    }
-                }
-            }
-            flag = 0;
+            if(res[idx] == -1) stack.push(idx);
         }
         return res;
     }
