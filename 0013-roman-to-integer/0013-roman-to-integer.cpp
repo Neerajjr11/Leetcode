8aa6adaa55a7d47979c7b63950c8ec3927799b4a
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-12-17 18:57:07 +0530

    Time: 10 ms (85.30%), Space: 6 MB (88.31%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.size();
+        int sumi = 0;
+        for(int i = n-1; i >= 0; i--){
+            if(s[i] == 'I')
+                sumi+=1;
+            else if(s[i] == 'V'){
+                if(i != 0){
+                if(s[i - 1] == 'I')
+                {
+                    sumi+=4;
+                    i--;
+                }
+                    else
+                        sumi +=5;
+                }
+                else
+                    sumi+=5;
+            }
+            else if(s[i] == 'X'){
+                if(i != 0){
+                if(s[i - 1] == 'I')
+                {
+                    sumi+=9;
+                    i--;
+                }
+                    else
+                        sumi +=10;
+                }
+                else
+                    sumi+=10;
+            }
+            else if(s[i] == 'L'){
+                if(i != 0){
+                if(s[i - 1] == 'X'){
+                    sumi+=40;
+                    i--;
+                }
+                    else
+                        sumi+=50;
+                }
+                else
+                    sumi+=50;
+            }
+            else if(s[i] == 'C'){
+                if(i != 0){
+                if(s[i - 1] == 'X'){
+                    sumi+=90;
+                    i--;
+                }
+                    else
+                        sumi+=100;
+                }
+                else
+                    sumi+=100;
+            }
+            else if(s[i] == 'D'){
+                if(i != 0){
+                if(s[i - 1] == 'C'){
+                    sumi+=400;
+                    i--;
+                }
+                    else
+                        sumi += 500;
+                }
+                else
+                    sumi+=500;
+            }
+            else if(s[i] == 'M'){
+                if(i != 0){
+                if(s[i - 1] == 'C'){
+                    sumi+=900;
+                    i--;
+                }
+                    else
+                        sumi += 1000;
+                }
+                else
+                    sumi+=1000;
+            }
+        }
+        return sumi;
+    }
+};
