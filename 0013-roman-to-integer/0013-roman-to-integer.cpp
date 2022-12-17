fa97f33f56e470a0880b33d15c3baa8ee04d59b9
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-12-17 18:57:49 +0530

    Time: 3 ms (98.74%), Space: 6.1 MB (67.24%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -8,74 +8,74 @@ public:
                 sumi+=1;
             else if(s[i] == 'V'){
                 if(i != 0){
-                if(s[i - 1] == 'I')
-                {
-                    sumi+=4;
-                    i--;
-                }
-                    else
-                        sumi +=5;
+                    if(s[i - 1] == 'I')
+                    {
+                        sumi+=4;
+                        i--;
+                    }
+                        else
+                            sumi +=5;
                 }
                 else
                     sumi+=5;
             }
             else if(s[i] == 'X'){
                 if(i != 0){
-                if(s[i - 1] == 'I')
-                {
-                    sumi+=9;
-                    i--;
-                }
-                    else
-                        sumi +=10;
+                    if(s[i - 1] == 'I')
+                    {
+                        sumi+=9;
+                        i--;
+                    }
+                        else
+                            sumi +=10;
                 }
                 else
                     sumi+=10;
             }
             else if(s[i] == 'L'){
                 if(i != 0){
-                if(s[i - 1] == 'X'){
-                    sumi+=40;
-                    i--;
-                }
-                    else
-                        sumi+=50;
+                    if(s[i - 1] == 'X'){
+                        sumi+=40;
+                        i--;
+                    }
+                        else
+                            sumi+=50;
                 }
                 else
                     sumi+=50;
             }
             else if(s[i] == 'C'){
                 if(i != 0){
-                if(s[i - 1] == 'X'){
-                    sumi+=90;
-                    i--;
-                }
-                    else
-                        sumi+=100;
+                    if(s[i - 1] == 'X'){
+                        sumi+=90;
+                        i--;
+                    }
+                        else
+                            sumi+=100;
                 }
                 else
                     sumi+=100;
             }
             else if(s[i] == 'D'){
                 if(i != 0){
-                if(s[i - 1] == 'C'){
-                    sumi+=400;
-                    i--;
-                }
-                    else
-                        sumi += 500;
+                    if(s[i - 1] == 'C'){
+                        sumi+=400;
+                        i--;
+                    }
+                        else
+                            sumi += 500;
                 }
                 else
                     sumi+=500;
             }
             else if(s[i] == 'M'){
                 if(i != 0){
-                if(s[i - 1] == 'C'){
-                    sumi+=900;
-                    i--;
-                }
-                    else
-                        sumi += 1000;
+                    if(s[i - 1] == 'C'){
+                        sumi+=900;
+                        i--;
+                    }
+                        else
+                            sumi += 1000;
                 }
                 else
                     sumi+=1000;
