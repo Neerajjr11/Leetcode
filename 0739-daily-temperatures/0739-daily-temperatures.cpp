7a835b37fc7a067d4b4311027c6ccbe21cb72e6c
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2022-12-18 10:18:22 +0530

    Time: 225 ms (85.30%), Space: 98.6 MB (89.86%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> stack;
-        vector<int> ans;
-        for(int i = temperatures.size() - 1; i >= 0; i--){
-            if(stack.empty()){
-                stack.push(i);
-                ans.push_back(0);
-            }
+        int hottest = INT_MIN;
+        vector<int> ans(temperatures.size(), 0);
+        for(int i = temperatures.size() - 1; i  >= 0; i--){
+            // if the temperature is greater than the hottest we store it no need to see the right side of our vector
+            // this removes the infinte loop if the index is 0
+            if(temperatures[i] >= hottest)
+                hottest = temperatures[i];
             else{
-                while(!stack.empty() && temperatures[stack.top()] <= temperatures[i])
-                    stack.pop();
-                if(stack.empty()){
-                    stack.push(i);
-                    ans.push_back(0);
-                }
-                else{
-                    ans.push_back(stack.top() - i);
-                    stack.push(i);
-                }
+                int j = i + 1;
+                // keep iteratiing till we find the particular temperature that is greater than the current one
+                while(temperatures[j] <= temperatures[i])
+                    j += ans[j];
+                // once the greater temp is found store the index of tht temp in the vector
+                ans[i] = j - i;
             }
         }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
