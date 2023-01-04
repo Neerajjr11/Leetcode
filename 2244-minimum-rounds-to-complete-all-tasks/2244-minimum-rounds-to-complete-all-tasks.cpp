78397b0627ce7476070302204c2d7840e3a5dd59
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-01-04 07:02:01 +0530

    Time: 244 ms (79.24%), Space: 103.5 MB (89.36%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -5,6 +5,9 @@ public:
         for(int i = 0; i < tasks.size(); i++){
             map[tasks[i]]++;
         }
+//         if a task repeats only once then it is impossible to complete ( return -1 )
+// Divide each frequency with the max completable tasks in a round (3)
+// check if there's tasks left, such that if one or two tasks are incomplete, we perform another round. ( +1 )
         int rounds = 0;
         for(auto it = map.begin(); it != map.end(); it++){
             if(it->second == 1)
