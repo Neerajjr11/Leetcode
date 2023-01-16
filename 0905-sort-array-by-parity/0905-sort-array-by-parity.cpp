12282acbb4568afc689cce6fc37d70c2b5c99367
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-01-16 16:16:34 +0530

    Time: 16 ms (52.79%), Space: 16.2 MB (84.10%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int i = 0;
-        int j = nums.size() - 1;
-        while(i<j){
-            if(nums[i] % 2 !=0){
-                swap(nums[i],nums[j]);
-                j--;
-                
-            }
-            else{
-            i++;}
+        // variation of of partition function in quick sort
+        if(nums.size() == 1)
+            return nums;
+        int i = 0, j = nums.size()-1;
+        while(i < j){
+            while(nums[i] % 2 == 0 && i < j) i++;
+            while(nums[j] % 2 != 0 && i < j) j--;
+            swap(nums[i], nums[j]);
         }
         return nums;
     }
