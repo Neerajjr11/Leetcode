907f51e6c26d8992c99c2f77d94e6211f364a4c7
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-01-30 18:56:47 +0530

    Time: 8 ms (24.47%), Space: 8.6 MB (43.25%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        vector<int>res;
-        int n = nums.size();
-        int sum = 0;
-        res.push_back(nums[0]);
-        for (int i=1;i<n;i++){
-            res.push_back(res.back() + nums[i]);
+        vector<int> prefixSum(nums.size());
+        prefixSum[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
-        return res;
+        return prefixSum;
     }
 };
