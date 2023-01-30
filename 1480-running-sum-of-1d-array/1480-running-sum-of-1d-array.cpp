2134273822206a7128e069883830cb6688ce7e4d
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-01-30 18:59:08 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (77.38%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> prefixSum(nums.size());
-        prefixSum[0] = nums[0];
         for(int i = 1; i < nums.size(); i++){
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+            nums[i] = nums[i - 1] + nums[i];
         }
-        return prefixSum;
+        return nums;
     }
 };
