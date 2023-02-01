fbd6b04843f792191f90c59393ed9b1a031337be
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-02-01 10:15:54 +0530

    Added solution - LeetHub

---
+++ First element to occur k times - GFG/first-element-to-occur-k-times.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstElementKTime(int a[], int n, int k)
+    {
+        map<int,int> mp;
+        for(int i = 0; i < n; i++){
+            mp[a[i]]++;
+            for(auto &i : mp){
+            if(i.second == k)
+                return i.first;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(a, n, k)<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
