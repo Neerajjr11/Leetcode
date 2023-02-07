be41b60fe844405fb0f5862cb516aef7968bbdf1
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-02-07 19:59:27 +0530

    Added solution - LeetHub

---
+++ Non-Repeating Element - GFG/non-repeating-element.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+        map<int, int> mp;
+        for(int i = 0; i < n; i++)
+            mp[arr[i]]++;
+        for(int i = 0; i < n; i++)
+            if(mp[arr[i]] == 1)
+                return arr[i];
+        return 0;
+    } 
+  
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.firstNonRepeating(arr,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
