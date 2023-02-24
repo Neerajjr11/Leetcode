a2228e6ddc08535728461cfd0ef070747f63ffc4
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-02-24 12:06:50 +0530

    Time: 3 ms (38.44%), Space: 5.9 MB (59.74%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    
+    bool isSameAfterReversals(int num) {
+        if(num==0)
+            return true;
+        if(num%10==0)
+            return false;
+        return true;
+    }
+};
