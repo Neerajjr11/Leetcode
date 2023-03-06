79c6ec8c166e892857d8857eb2399ac447871fd2
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-03-06 12:07:38 +0530

    Time: 30 ms (41.45%), Space: 8.9 MB (41.00%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,14 +7,12 @@ public:
         int res = 0;
         while(j < n){
             mp[s[j]]++;
-            if((j - i + 1) < mp.size())
-                j++;
-            else if(mp.size() == (j - i  + 1)){
+            if(mp.size() == (j - i  + 1)){
                 res = max(res, (j - i + 1));
                 j++;
             }
             else{
-                while((j - i + 1) > mp.size()){
+                while(mp.size() < (j - i + 1)){
                     mp[s[i]]--;
                     if(mp[s[i]] == 0)
                         mp.erase(s[i]);
