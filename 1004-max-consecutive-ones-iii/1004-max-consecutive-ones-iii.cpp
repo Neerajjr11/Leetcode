b042f4118fe32c955beb5d356801766c2018ac85
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-03-07 22:28:19 +0530

    Time: 61 ms (78.57%), Space: 55.5 MB (21.01%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+        // count the number of zeros in the var sliding window
+        // if the zeros are less than or equal to k then compare the window size with maximum window
+        // size seen till now and output the max window size with at most k zeros
         int res = 0;
         int i = 0, j = 0, n = nums.size(), countzero = 0;
         while(j < n){
