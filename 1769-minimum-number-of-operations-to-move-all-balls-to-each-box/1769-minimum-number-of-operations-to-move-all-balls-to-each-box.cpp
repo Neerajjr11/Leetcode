146c059047866c28edda3f41132cb376f4236a29
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-03-21 16:53:46 +0530

    Time: 6 ms (87.12%), Space: 9.5 MB (21.68%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> res(boxes.size(), 0);
-        for(int i = 0; i < boxes.length(); i++){
-            for(int j = 0; j < boxes.length(); j++){
-                if(boxes[j] == '1')
-                    res[i] += abs(i - j);
-            }
+        int n = boxes.size();
+        int count;
+        vector<int> res(n, 0);
+        vector<int> pre(n, 0);
+        vector<int> suff(n, 0);
+        count = boxes[0] - '0';
+        for(int i = 1; i < n; i++){
+            pre[i] = pre[i - 1] + count;
+            count += boxes[i] - '0';
         }
+        count = boxes[n - 1] - '0';
+        for(int i = n - 2; i >= 0; i--){
+            suff[i] = suff[i + 1] + count;
+            count += boxes[i] - '0';
+        }
+        for(int i = 0; i < n; i++)
+            res[i] = pre[i] + suff[i];
         return res;
     }
 };
