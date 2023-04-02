80908a0e58d2e48f1de9365c0b314a38f79d734f
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-04-02 10:38:57 +0530

    Time: 278 ms (22.96%), Space: 99.9 MB (31.79%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        ios_base::sync_with_stdio(false);
+        using ll = long long;
         vector<int> res;
         int i = 0;
         int n = potions.size();
         sort(begin(potions), end(potions));
         while(i < spells.size()){
             int low = 0;
-            int high = potions.size() - 1;
-            int found = INT_MAX;
-            while(low <= high){
-                int mid = low + (high - low) / 2;
-                if((long long) potions[mid]*spells[i] >= success){
-                    found = mid;
-                    high = mid - 1;
-                }
-                else
-                    low = mid + 1;
+            int high = potions.size();
+            
+            while(low < high) {
+                int mid = (low + high)/2;
+                
+                ll val = (ll)potions[mid] * spells[i];
+                
+                if(val >= success) high = mid;
+                else low = mid + 1;
             }
-            // cout<<found<<" ";
-            if(found != INT_MAX)
-                res.push_back(n - found);
-            else
-                res.push_back(0);
+            res.push_back(n-low);
             i++;
         }
         return res;
