0f51ac5b24994887cbd923f7e1d0d4425574550b
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-04-21 22:49:06 +0530

    Time: 294 ms (34.91%), Space: 53.3 MB (33.49%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+
+        int MOD = 1e9 + 7;
+        int size = group.size();
+        vector<vector<vector<int>>> dp(size + 1, vector<vector<int>>(minProfit + 1, vector<int>(n + 1, 0)));
+        dp[0][0][0] = 1;
+        for (int i = 1; i <= size; i++) {
+            int g = group[i - 1];
+            int p = profit[i - 1];
+            for (int j = 0; j <= minProfit; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (k >= g) {dp[i][j][k] = (dp[i][j][k] + dp[i - 1][max(0, j - p)][k - g]) % MOD;
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for (int k = 0; k <= n; k++) {
+            ans = (ans + dp[size][minProfit][k]) % MOD;
+        }
+        return ans;
+    }
+};
