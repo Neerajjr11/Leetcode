2f1cb98850eaa4dcbd3f6b3c4d3f96242a3d4e86
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-07-23 08:28:01 +0530

    Time: 14 ms (37.49%), Space: 17.1 MB (30.43%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
+        vector<vector<int>> res;
+        vector<int> helper;
         int n = candidates.size();
-        vector<int>helper;
-        function<void(int,int)> solve = [&](int ind, int s) {
+        
+        function<void(int, int)> solve = [&](int ind, int target){
+            // base case
             if(ind == n){
-                if(s == 0)
+                if(target == 0)
                     res.push_back(helper);
                 return;
             }
-            if(candidates[ind] <= s){
+            
+            // PICKING of a number case
+            if(candidates[ind] <= target){
+                // add the number to the vector
                 helper.push_back(candidates[ind]);
-                s -= candidates[ind];
-                solve(ind, s);
-                s += helper[helper.size() - 1];
+                // dont increment index as duplication is possible
+                solve(ind, target - candidates[ind]);
+                // the reason we are doing this as well in the loop is because there is a possiblity of candidates[ind] <= target but total crossing the target eg.
+                // 2,2,2,2.2 = 8 so we need to remove the 2
                 helper.pop_back();
             }
-            solve(ind + 1, s);
-        };      
-        
-        solve(0,target);
+            solve(ind + 1, target);
+        };
+        solve(0, target);
         return res;
     }
 };
