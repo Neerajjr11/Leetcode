e5fcbcd56a5449f441368df8c5e9c8880f2c5aa9
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-07-30 14:14:40 +0530

    Time: 116 ms (82.30%), Space: 53.1 MB (80.36%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool isPalindrome(const string& s, int start, int end) {
-        while(start <= end) {
-            if(s[start++] != s[end--])
-                return false;
-        }
-        return true;
+    bool isPalindrome(string s){
+        string str = s;
+        reverse(s.begin(), s.end());
+        return s == str;
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string> > res;
-        if(s.empty())
-            return res;
-        vector<string> path;
-        function<void(int)>dfs = [&](int index){
-            if(index == s.size()) {
-            res.push_back(path);
-            return;
+        vector<vector<string>> res;
+        vector<string> helper;
+        
+        function<void(int , string)> solve = [&](int ind, string s){
+            if(ind == s.size()){
+                res.push_back(helper);
+                return;
             }
-            for(int i = index; i < s.size(); ++i) {
-                if(isPalindrome(s, index, i)) {
-                    path.push_back(s.substr(index, i - index + 1));
-                    dfs(i+1);
-                    path.pop_back();
+            for(int j = ind; j < s.size(); j++){
+                if(isPalindrome(s.substr(ind, j - ind + 1))){
+                    helper.push_back(s.substr(ind, j - ind + 1));
+                    solve(j + 1, s);
+                    helper.pop_back();
                 }
             }
         };
-        dfs(0);
+        solve(0, s);
         return res;
     }
 };
