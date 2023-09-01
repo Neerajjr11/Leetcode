13c3c45ad1344e5e65d1b63345ffbd753612816b
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-09-01 08:07:46 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (82.66%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
+    void reverseCharacters(string& s, int startIndex, int endIndex){
+        while(startIndex <= endIndex){
+            swap(s[startIndex], s[endIndex]);
+            startIndex++;
+            endIndex--;
+        }
+    }
+    string cleanSpace(string a, int n){
+        int i = 0, j = 0;
+      
+        while (j < n) {
+          while (j < n && a[j] == ' ') j++;             // skip spaces
+          while (j < n && a[j] != ' ') a[i++] = a[j++]; // keep non spaces
+          while (j < n && a[j] == ' ') j++;             // skip spaces
+          if (j < n) a[i++] = ' ';                      // keep only one space
+        }
+        return a.substr(0, i);
+    }
     string reverseWords(string s) {
-        string word = "";
-        vector<string> store;
-        string res = "";
-        for(int i =0;i<s.length();i++){
-            string word=""; 
-            if(s[i]==' ')continue;
-            while(s[i]!=' ' and i<s.length()){
-                word+=s[i];
-                i++;
+        // Reverse all the characters in the entire message, giving us the correct word order but with each word backward.
+        // Reverse the characters in each individual word.
+        if(s.empty())
+            return "";
+        // to remove and leading and trailing whitespaces
+        s = cleanSpace(s, s.size());
+        // reverse characters of the sentence
+        reverseCharacters(s, 0 , s.size() - 1);
+        // cout << s;
+        int currentWordStartIndex = 0;
+        
+        // reverse the characters in individual word
+        for(int i = 0; i <= s.size(); i++){
+            if(i == s.size() || s[i] == ' '){
+                reverseCharacters(s, currentWordStartIndex, i - 1);
+                currentWordStartIndex = i + 1;
             }
-            store.push_back(word);
         }
-        for(int i = store.size() - 1;i >= 1;i--){
-            res+=store[i];
-            res+=" ";
-        }
-        res+=store[0];
-        return res;
+        return s;
     }
 };
