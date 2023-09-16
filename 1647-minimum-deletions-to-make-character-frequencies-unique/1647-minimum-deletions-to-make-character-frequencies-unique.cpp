068ff13a85bcc86f03c4417b87ed1bb892dfaf9e
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-09-16 21:35:13 +0530

    Time: 106 ms (15.07%), Space: 17.8 MB (9.18%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -6,13 +6,12 @@ public:
         int res = 0;
         for(auto&x : s)
             mp[x]++;
-        // for(auto& i : mp)
-        //     cout<<i.first<<" "<<i.second<<" ";
         for(auto& i : mp){
             while(st.count(i.second)){
                 i.second--;
                 res++;
             }
+            // This is an edge case as when the count of frequency of characters is 0, we don't need to add it as it doesn't appears in the string at all
             if(i.second != 0)
                 st.insert(i.second);
         }
