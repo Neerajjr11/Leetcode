7d1f570dbc2a5a9e28fe526b4c7286a5d302d3eb
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-11-18 00:18:04 +0530

    Time: 166 ms (78.29%), Space: 63.6 MB (76.76%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<double, vector<int>>> res;
-        for(int i = 0; i < points.size(); i++){
-            double rt = sqrt(pow(points[i][0], 2) + pow(points[i][1], 2));
-            res.push_back(make_pair(rt,points[i]));
+        // using max heap since we need to get the closest or in this scenario k lowest distance 
+        priority_queue<pair<int, pair<int,int>>> pq;
+        for(auto& i : points){
+            // for simple calc we can take (x ^ 2 + y ^2) instead of sqrt to maintain the int
+            
+            pq.push({(i[0]*i[0] + i[1] * i[1]), {i[0], i[1]}});
+            if(pq.size() > k)
+                pq.pop();
         }
-        sort(begin(res), end(res));
-        vector<vector<int>> ans;
+        vector<vector<int>> res;
         for(int i = 0; i < k; i++){
-            ans.push_back(res[i].second);
+            pair<int,int> p = pq.top().second;
+            res.push_back({p.first, p.second});
+            pq.pop();
         }
-        return ans;
+        return res;
     }
 };
