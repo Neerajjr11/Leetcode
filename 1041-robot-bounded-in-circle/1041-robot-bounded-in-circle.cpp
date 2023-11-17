6d41056f6a004a90a629450e2621bdb59a8aefb6
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-11-17 22:43:33 +0530

    Time: 3 ms (35.77%), Space: 6.4 MB (75.24%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -4,43 +4,54 @@ public:
         char dir = 'N';
         int x = 0;
         int y = 0;
-        for(int i = 0; i < 1000; i++){
-            for(auto&i : instructions){
-                if(i == 'G'){
-                    if(dir == 'N')
-                        y++;
-                    else if(dir == 'S')
-                        y--;
-                    else if(dir == 'E')
-                        x++;
-                    else
-                        x--;
-                }
-                else if(i == 'L'){
-                    if(dir == 'N')
-                        dir = 'W';
-                    else if(dir == 'S')
-                        dir = 'E';
-                    else if(dir == 'E')
-                        dir = 'N';
-                    else 
-                        dir = 'S';
-                }
-                else if(i == 'R'){
-                    if(dir == 'N')
-                        dir = 'E';
-                    else if(dir == 'S')
-                        dir = 'W';
-                    else if(dir == 'E')
-                        dir = 'S';
-                    else
-                        dir = 'N';
-                }
-            
+        int has_rotated = 0;
+        for(auto&i : instructions){
+            if(i == 'G'){
+                if(dir == 'N')
+                    y++;
+                else if(dir == 'S')
+                    y--;
+                else if(dir == 'E')
+                    x++;
+                else
+                    x--;
             }
-            if(x == 0 and y == 0)
-                return true;
+            else if(i == 'L'){
+//                     we add 1 based on the map drawn below
+                has_rotated = (has_rotated + 1);
+                if(dir == 'N')
+                    dir = 'W';
+                else if(dir == 'S')
+                    dir = 'E';
+                else if(dir == 'E')
+                    dir = 'N';
+                else 
+                    dir = 'S';
+            }
+            else if(i == 'R'){
+//                     we add 3 based on the map drawn below as 1 right is equal to 3 left turns
+                has_rotated = (has_rotated + 3);
+                if(dir == 'N')
+                    dir = 'E';
+                else if(dir == 'S')
+                    dir = 'W';
+                else if(dir == 'E')
+                    dir = 'S';
+                else
+                    dir = 'N';
+            }
+
         }
-        return false;
+        // we mod by 4 to check if we have covered a full circle so if we have covered a full circle and still haven't reached x = 0 and y = 0 we will reach it at some future point as the robot repeats the instruction forever
+        if(x == 0 and y == 0 ||  (has_rotated % 4) != 0)
+            return true;
+        else
+            return false;
     }
 };
+
+   //      N (0)
+   //         |
+   // W (1) ------ E (3)
+   //         |
+   //      S (2)
