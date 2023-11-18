82a2ae23e9460598ee80f0f4e8f0966e025ea7f1
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-11-18 15:42:52 +0530

    Time: 11 ms (65.25%), Space: 20.8 MB (23.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> prefixMax(n, 0);
-        prefixMax[0] = height[0];
-        for(int i = 1; i < n - 1; i++)
-            prefixMax[i] = max(height[i] , prefixMax[i - 1]);
-        vector<int> suffixMax(n, 0);
-        suffixMax[n - 1] = height[n - 1];
-        for(int i = n - 2; i >= 0; i--)
-            suffixMax[i] = max(height[i] , suffixMax[i + 1]);
-        int res = 0;
-        for(int i = 1; i < n - 1; i++)
-            res += (min(prefixMax[i], suffixMax[i]) - height[i]);
-        return res;
-        
+        // to get the max of height in left array to the element
+        vector<int> prefix_mheight(n, 0);
+        prefix_mheight[0] = height[0];
+        for(int i = 1; i < n; i++){
+            prefix_mheight[i] = max(height[i], prefix_mheight[i - 1]);
+        }
+        // to get the max of height in right array to the element
+        vector<int> suffix_mheight(n, 0);
+        suffix_mheight[n - 1] = height[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            suffix_mheight[i] = max(height[i], suffix_mheight[i + 1]);
+        }
+        int total_water = 0;
+        for(int i = 0; i < n; i++){
+            total_water += (min(prefix_mheight[i], suffix_mheight[i]) - height[i]);
+        }
+        return total_water;
     }
 };
