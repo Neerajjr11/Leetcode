770849825c2b88f252d18e01e3dcb73f867c0f40
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-11-18 16:24:01 +0530

    Time: 20 ms (48.45%), Space: 16.5 MB (92.00%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,35 +1,87 @@
+// with extra space
+// class MinStack {
+// public:
+//     stack<int> s;
+//     stack<int> ss;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         s.push(val);
+//         if(ss.empty())
+//             ss.push(val);
+//         else if(ss.top() >= val)
+//             ss.push(val);
+//     }
+    
+//     void pop() {
+//         int x = s.top();
+//         s.pop();
+//         if(x == ss.top())
+//             ss.pop();
+//     }
+    
+//     int top() {
+//         return s.top();
+//     }
+    
+//     int getMin() {
+//         return ss.top();
+//     }
+// };
+
+// with O(1) space
 class MinStack {
 public:
-    stack<int> s;
-    stack<int> ss;
+    stack<long long> s;
+    long long min_ele;
+    
     MinStack() {
-        
+        min_ele = INT_MAX;
     }
     
     void push(int val) {
-        s.push(val);
-        if(ss.empty())
-            ss.push(val);
-        else if(ss.top() >= val)
-            ss.push(val);
+        if (s.empty()) {
+            s.push(val);
+            min_ele = val;
+        } else {
+            if (val >= min_ele) {
+                s.push(val);
+            } else {
+                s.push(2LL * val - min_ele);
+                min_ele = val;
+            }
+        }
     }
     
     void pop() {
-        int x = s.top();
+        if (s.empty()) return;
+
+        long long top = s.top();
         s.pop();
-        if(x == ss.top())
-            ss.pop();
+
+        if (top < min_ele) {
+            min_ele = 2LL * min_ele - top;
+        }
     }
     
     int top() {
-        return s.top();
+        if (s.empty()) return -1;
+
+        long long top = s.top();
+        if (top < min_ele) {
+            return min_ele;
+        }
+        return top;
     }
     
     int getMin() {
-        return ss.top();
+        if (s.empty()) return -1;
+
+        return min_ele;
     }
 };
-
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
