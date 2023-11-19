f724a76af8f9dcd288af219821650659ac345905
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-11-19 09:05:01 +0530

    Time: 39 ms (25.24%), Space: 11.2 MB (15.95%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,13 +1,30 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         unordered_map<char, int> mp;
+//         for(auto& i : s)
+//             mp[i]++;
+//         for(int i = 0; i < s.size(); i++){
+//             if(mp[s[i]] == 1)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> mp;
-        for(auto& i : s)
-            mp[i]++;
+        unordered_map<char, pair<int, int> > mp;
         for(int i = 0; i < s.size(); i++){
-            if(mp[s[i]] == 1)
-                return i;
+            mp[s[i]].first++;
+            mp[s[i]].second = i;
         }
-        return -1;
+        int idx = INT_MAX;
+        for(auto& i : mp){
+            if(i.second.first == 1)
+                idx = min(idx, i.second.second);
+        }
+        return idx == INT_MAX ? -1 : idx;
     }
 };
