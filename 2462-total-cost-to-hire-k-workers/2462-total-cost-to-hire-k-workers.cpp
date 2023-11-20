bb8e717323c9d4d6beadd2d330e996f553790bd9
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-11-20 16:53:56 +0530

    Time: 133 ms (52.80%), Space: 73.2 MB (51.35%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        long long n = costs.size();
-        long long oflow = n - candidates;
-        priority_queue<int, vector<int>, greater<int> > p1;
-        int i=0;
-        while(i<candidates){
-            p1.push(costs[i]);
-            i++;
-        }
-        if(candidates >  oflow){
-            candidates = oflow;
-        }
-        priority_queue<int, vector<int>, greater<int> > p2;
-        long long c = 0;
-        long long j = n-1;
-        while(c<candidates){
-            p2.push(costs[j]);
-            c++;
-            j--;
-        }
-        long long ans = 0;
-        while(k--){  
-            if(p1.size() == 0){
-                ans += p2.top();
-                p2.pop();
-                if(i <= j){
-                   p2.push(costs[j]);
-                    j--;
-                } 
+        priority_queue<int,vector<int>,greater<int>> pq1; 
+        priority_queue<int,vector<int>,greater<int>> pq2; 
+        long long cost = 0;
+        int top1, top2;
+        int l = 0, r = costs.size() - 1;
+        while(k--){
+            while(pq1.size()<candidates and l <=r)
+                pq1.push(costs[l++]);
+            while(pq2.size() < candidates and l <= r)
+                pq2.push(costs[r--]);
+            if(pq1.size() > 0)
+                top1 = pq1.top();
+            else
+                top1 = INT_MAX;
+            if(pq2.size() > 0)
+                top2 = pq2.top();
+            else
+                top2 = INT_MAX;
+            if(top1 <= top2){
+                pq1.pop();
+                cost += top1;
+            }else{
+                pq2.pop();
+                cost += top2;
             }
-            else if(p2.size() == 0 || p1.top() <= p2.top()){
-                ans += p1.top();
-                p1.pop();
-                if(i <= j){
-                    p1.push(costs[i]);
-                    i++;
-                } 
-            }
-            else{
-                ans += p2.top();
-                p2.pop();
-                if(i <= j){
-                 p2.push(costs[j]);
-                    j--;
-                } 
-            }
+        
         }
-        return ans;
+        return cost;
     }
 };
