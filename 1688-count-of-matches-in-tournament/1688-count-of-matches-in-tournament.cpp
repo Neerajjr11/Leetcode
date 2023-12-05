66788b496ffd1d07a667622820eedafe22c7bf25
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-12-05 08:17:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (73.09%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -4,11 +4,11 @@ public:
         int res = 0;
         while(n > 1){
             if(n % 2 == 0){
-                res += (n / 2);
+                res += n / 2;
                 n /= 2;
             }else{
-                res += ((n - 1) / 2);
-                n = (((n - 1) / 2) +  1);
+                res += ((n - 1) / 2) + 1;
+                n = (n - 1) / 2;
             }
         }
         return res;
