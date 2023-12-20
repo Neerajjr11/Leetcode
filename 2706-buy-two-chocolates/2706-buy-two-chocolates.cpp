c07c20bca37985d2dbe3315e95d98121a7102d5b
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2023-12-20 13:06:55 +0530

    Time: 20 ms (13.52%), Space: 46.9 MB (94.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -5,9 +5,23 @@ public:
 //        If the sum is less then money then return (money - sum)
 //        else return money
         // using sorting O(N * logN)
-        sort(prices.begin(), prices.end());
-        if(prices[0] + prices[1] > money)
+        // sort(prices.begin(), prices.end());
+        // if(prices[0] + prices[1] > money)
+        //     return money;
+        // return (money - (prices[0] + prices[1]));
+        
+        // finding the smallest and second smallest in O(N)
+        int smallest = INT_MAX, secondSmallest = INT_MAX;
+        for(auto& x : prices){
+            if(smallest > x){
+                secondSmallest = smallest;
+                smallest = x;
+            }
+            else if(secondSmallest >= x)
+                secondSmallest = x;
+        }
+        if(smallest + secondSmallest > money)
             return money;
-        return (money - (prices[0] + prices[1]));
+        return (money - (secondSmallest + smallest));
     }
 };
