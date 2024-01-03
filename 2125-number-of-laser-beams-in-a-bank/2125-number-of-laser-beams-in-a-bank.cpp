58a23b8e44bf8adb69397e5ceca58f41d9f2178e
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2024-01-03 12:36:22 +0530

    Time: 166 ms (6.71%), Space: 23.4 MB (82.87%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,28 +1,47 @@
+// class Solution {
+// public:
+//     int numberOfBeams(vector<string>& bank) {
+//         int totalBeams = 0;
+//         vector<int>helper;
+//         for(int i = 0; i < bank.size(); i++){
+//             int totalSecurity = 0;
+//             for(int j = 0; j < bank[0].size(); j++){
+//                 if(bank[i][j] == '1')
+//                     totalSecurity++;
+//             }
+//             helper.push_back(totalSecurity);
+//         }
+//         int i = 0, j = 1;
+//         while(j < helper.size()){
+//             if(helper[j] == 0)
+//                 j++;
+//             else if(helper[i] == 0)
+//                 i++;
+//             else if(i != j){
+//                 totalBeams += (helper[i] * helper[j]);
+//                 i++;
+//                 j++;
+//             }else
+//                 j++;
+//         }
+//         return totalBeams;
+//     }
+// };
+
+// O(m * n) solution
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int totalBeams = 0;
-        vector<int>helper;
-        for(int i = 0; i < bank.size(); i++){
-            int totalSecurity = 0;
-            for(int j = 0; j < bank[0].size(); j++){
-                if(bank[i][j] == '1')
-                    totalSecurity++;
+        int totalBeams = 0, prevSecurityDevices = 0, currSecDev = 0;
+        for(auto& s : bank){
+             currSecDev = 0;
+            for(auto& c : s)
+                if(c == '1')
+                    currSecDev++;
+            if(currSecDev){
+                totalBeams += (prevSecurityDevices * currSecDev);
+                prevSecurityDevices = currSecDev;
             }
-            helper.push_back(totalSecurity);
-        }
-        int i = 0, j = 1;
-        while(j < helper.size()){
-            if(helper[j] == 0)
-                j++;
-            else if(helper[i] == 0)
-                i++;
-            else if(i != j){
-                totalBeams += (helper[i] * helper[j]);
-                i++;
-                j++;
-            }else
-                j++;
         }
         return totalBeams;
     }
