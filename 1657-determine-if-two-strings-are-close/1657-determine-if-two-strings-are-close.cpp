d2ff5cd74d836e8284020fbc4c80942f59f66550
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2024-01-14 10:42:50 +0530

    Time: 79 ms (77.44%), Space: 20.9 MB (89.19%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.length() != word2.length()) return false;
-        
-        vector<int> w1_freq(26,0);
-        
-        vector<int> w2_freq(26,0);
-        
-        for(int i = 0; i < word1.length(); i++)
-            w1_freq[word1[i] - 'a']++;
-        
-        for(int i = 0; i < word2.length(); i++){
-            if(w1_freq[word2[i] - 'a'] == 0)
+        vector<int> v1(26,0);
+        vector<int> v2(26, 0);
+        for(int i = 0; i < word1.size(); i++)
+            v1[word1[i] - 'a']++;
+        for(int i = 0; i < word2.size(); i++){
+            if(v1[word2[i] - 'a'] == 0)
                 return false;
-            w2_freq[word2[i] - 'a']++;
+            v2[word2[i] - 'a']++;
         }
+        sort(v1.begin(), v1.end());
+        sort(v2.begin(), v2.end());
+        return v1 == v2;
         
-        sort(w1_freq.begin(), w1_freq.end());
-        sort(w2_freq.begin(), w2_freq.end());
-        
-        return(w1_freq == w2_freq);
-
     }
 };
