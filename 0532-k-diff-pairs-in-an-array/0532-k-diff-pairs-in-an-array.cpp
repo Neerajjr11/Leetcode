f6727df97874274a7bc6290a32c94976cc03dafb
Neeraj_jm <50861092+Neerajjr11@users.noreply.github.com>
2024-04-12 13:58:53 +0530

    Time: 13 ms (63.28%), Space: 16.8 MB (15.10%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
+        int ans = 0;
         for(auto i : nums)
             mp[i]++;
-        int count = 0;
         for(auto i : mp){
             if(k == 0){
-                if(i.second > 1)
-                    count++;
-            }else{
-                if(mp.find(i.first + k) != mp.end())
-                    count++;
+                // here if k is 0 we need to find 2 elements which are same, and doesn't matter if the elements freq is more than 2 as we need to find unique pairs
+                if(i.second >= 2)
+                    ans++;
             }
+            else
+                if(mp.find(i.first + k) != mp.end())
+                    ans++;
         }
-        return count;
+        return ans;
     }
 };
